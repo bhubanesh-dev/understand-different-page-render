8bd149a2a2a227147b42c48d2baaac7178ec9b98
bhubanesh-dev <bhubanesh.dev@gmail.com>
2025-08-03 00:25:57 +0530

    Remove custom api

--- app/api/welcome/[renderType]/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextRequest, NextResponse } from 'next/server';
-
-export async function GET(
-  request: NextRequest,
-  { params }: { params: Promise<{ renderType: string }> }
-) {
-  const { renderType } = await params;
-  return NextResponse.json({ message: `This page is rendered as: ${renderType}` })
-}
