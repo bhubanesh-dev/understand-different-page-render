ff6a7ed6bce35b6d4a46c85f16d0d3e5ae483ab3
bhubanesh-dev <bhubanesh.dev@gmail.com>
2025-08-03 00:02:06 +0530

    change the route.ts IV

--- app/api/welcome/[renderType]/route.ts
+++ app/api/welcome/[renderType]/route.ts
@@ -2,7 +2,8 @@ import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(
   request: NextRequest,
-  { params }: { params: { renderType: string } }
+  { params }: { params: Promise<{ renderType: string }> }
 ) {
-  const { renderType } = params;
-  return NextResponse.json({ message: `This page is rendered as: ${renderType}` });
+  const { renderType } = await params;
+  return NextResponse.json({ message: `This page is rendered as: ${renderType}` })
+}
