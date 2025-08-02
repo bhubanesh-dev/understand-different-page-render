4145821cf0de969608566e32c338fe132c33500d
bhubanesh-dev <bhubanesh.dev@gmail.com>
2025-08-02 23:51:30 +0530

    change the route.ts II

--- app/api/welcome/[renderType]/route.ts
+++ app/api/welcome/[renderType]/route.ts
@@ -1,11 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 
-export async function GET(
-  request: NextRequest,
-  context: { params: { renderType: string } }
-) {
+export async function GET(req: NextRequest, context: { params: { renderType: string } }) {
   const { renderType } = context.params;
-  return NextResponse.json({
-    message: `This page is rendered as: ${renderType}`,
-  });
+  return NextResponse.json({ message: `This page is rendered as: ${renderType}` });
 }
