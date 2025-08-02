f630fcd0cf341e14dab3a5ca5af6d1cbb398f244
bhubanesh-dev <bhubanesh.dev@gmail.com>
2025-08-02 23:38:10 +0530

    change the route.ts

--- app/api/welcome/[renderType]/route.ts
+++ app/api/welcome/[renderType]/route.ts
@@ -1,9 +1,11 @@
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(
-  request: Request,
-  { params }: { params: { renderType: string } }
+  request: NextRequest,
+  context: { params: { renderType: string } }
 ) {
-  const { renderType } = params || { renderType: "Guest" };
-  return NextResponse.json({ message: `This page is rendered as: ${renderType}` });
+  const { renderType } = context.params;
+  return NextResponse.json({
+    message: `This page is rendered as: ${renderType}`,
+  });
 }
