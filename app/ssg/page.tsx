8cde84b46708657ee0e8d0eb68b39a3d80458e62
bhubanesh-dev <bhubanesh.dev@gmail.com>
2025-08-03 00:14:39 +0530

    changes in ssg page.tsx

--- app/ssg/page.tsx
+++ app/ssg/page.tsx
@@ -2,14 +2,14 @@ export default async function SSGPage() {
   const time = new Date().toLocaleString();
 
   
-  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/welcome/ssg`)
+  const response = await fetch(`https://api.vercel.app/blog`)
   const data = await response.json();
 
   return (
     <main>
       <h1 className="text-2xl font-bold">SSG Page</h1>
       <p className="text-lg font-semibold">This page was statically generated at: {time}</p>
-      <p>{data.message}</p>
+      <p>sample data that fetched from another url: {data[0].title}</p>
     </main>
   );
 }
