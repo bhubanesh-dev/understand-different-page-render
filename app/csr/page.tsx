a0090d79193e14f8b968c6b73d460c9556f4052a
bhubanesh-dev <bhubanesh.dev@gmail.com>
2025-08-03 00:22:25 +0530

    change the api call

--- app/csr/page.tsx
+++ app/csr/page.tsx
@@ -3,16 +3,16 @@ import { useEffect, useState } from 'react';
 
 export default function SSRPage() {
 
-    const [data, setData] = useState<{ message: string } | null>(null);
+    const [data, setData] = useState("");
     const [loading, setLoading] = useState(true);
     const [time, setTime] = useState('');
 
     useEffect(() => {
         setTime(new Date().toLocaleString());
         (async () => {
-            const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/welcome/ssr`);
+            const res = await fetch(`https://api.vercel.app/blog`);
             const json = await res.json();
-            setData(json);
+            setData(json[0].title);
             setLoading(false);
         })();
     }, []);
@@ -21,7 +21,7 @@ export default function SSRPage() {
         <main>
             <h1 className="text-2xl font-bold"> CSR Page</h1>
             <p className="text-lg font-semibold mb-4">This page was rendered at: {time}</p>
-            {loading ? <p className="text-gray-300 ">Loading message...</p> : <p>{data?.message}</p>}
+            {loading ? <p className="text-gray-300 ">Loading message...</p> : <p>{data}</p>}
         </main>
     );
 }
--- app/ssr/page.tsx
+++ app/ssr/page.tsx
@@ -1,14 +1,14 @@
-export const dynamic = 'force-dynamic';  //check route segment config : https://nextjs.org/docs/app/api-reference/file-conventions/route-segment-config
+// export const dynamic = 'force-dynamic';  //check route segment config : https://nextjs.org/docs/app/api-reference/file-conventions/route-segment-config
 
 export default async function SSRPage() {
     const time = new Date().toLocaleString();
-    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/welcome/ssr`, { cache: 'no-store' })
+    const response = await fetch(`https://api.vercel.app/blog`, { cache: 'no-store' })
     const data = await response.json();
     return (
         <main>
             <h1 className="text-2xl font-bold">SSR Page</h1>
             <p className="text-lg font-semibold">This page was server-side rendered at: {time}</p>
-            <p>{data.message}</p>
+            <p>{data[0].title}</p>
         </main>
     );
 }
